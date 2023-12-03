6647f7b21195ba607f5f3c798cc7683d9fd22899
Anchal <Anchal2011030@akgec.ac.in>
2023-12-03 17:05:14 +0530

    made a c++ folder and put all programs into it

---
+++ C++_Programs/Loop.cpp
@@ -0,0 +1,62 @@
+// 1. Write a program to print “Bright IT Career” ten times using for loop
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<"Bright IT Career"<<endl;
+    }
+}
+
+
+
+// 2. Write a java program to print 1 to 20 numbers using the while loop.
+int main(){
+    int i=1;
+    while(i<=20){
+        cout<<i<<endl;
+        i++;
+    }
+}
+
+
+
+// 3. Program to equal operator and not equal operators
+int main(){
+        int x=7;
+        if(i==7) 
+        cout<<"Its seven"<<endl;
+        else if(i!=7) cout<<"Its ten"<<endl;
+}
+
+
+
+// 4. Write a program to print the odd and even numbers.
+int main(){
+    for(int i=1;i<=10;i++){
+        if(i%2==0)
+        cout<<i: <<"even number"<<endl;
+        else if(i%2!=0)
+        cout<<i: <<"odd number"<<endl;
+    }
+}
+
+
+
+// 5. Write a program to print largest number among three numbers.
+int main(){
+    int x=3,y=1,z=8;
+    if()
+    for(int i=1;i<=10;i++){
+        if(i%2==0)
+        cout<<i: <<"even number"<<endl;
+        else if(i%2!=0)
+        cout<<i: <<"odd number"<<endl;
+    }
+}
+
+
+// 6. Write a program to print even number between 10 and 20 using while
+// 7. Write a program to print 1 to 10 using the do-while loop statement.
+// 8. Write a program to find Armstrong number or not
+// 9. Write a program to find the prime or not.
+// 10. Write a program to palindrome or not.
+// 11. Program to check whether a number is EVEN or ODD using switch
+// 12. Print gender (Male/Female) program according to given M/F using switc
